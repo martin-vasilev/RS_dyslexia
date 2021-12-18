90b7ca59e31f3fd8d3f498eeb2d34646f2b645db
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2021-12-18 12:23:29 +0000

    Update prep_asc.py

--- preproc/prep_asc.py
+++ preproc/prep_asc.py
@@ -8,10 +8,10 @@ Created on Fri Dec 17 16:11:06 2021
 import re
 
 file_dir= 'D:/Data/Dyslexia/'
-file_name= '3.asc'
+file_name= '3'
 
 # open the .asc file
-with open(file_dir + file_name) as file:
+with open(file_dir + file_name + '.asc') as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
     
@@ -24,6 +24,7 @@ cond= 0
 item= 0
 ET_flag= ''
 wait_flag=0 
+trial_seq= 0
 
 
 for i in range(len(lines)):
@@ -37,6 +38,7 @@ for i in range(len(lines)):
     if 'IMGLOAD CENTER' in curr_line: # image loaded for current trial
         img_file= curr_line.split(' ')[4]
         wait_flag= 1
+        trial_seq= trial_seq+1
         
         if 'text' in img_file: # check if it is item
             D= 0
@@ -56,9 +58,22 @@ for i in range(len(lines)):
             item= int(re.findall(r'\d+', img_file)[1])
         
         # if item is a question, take last item number (since item is not updated above)
-
+        ET_flag= 'TRIALID ' + 'E' + str(cond)+'I' +str(item)+ 'D' +str(D)
     
-    ET_flag= 'TRIALID ' + 'E' + str(cond)+'I' +str(item)+ 'D' +str(D)
+
+    if 'TRIALID' in curr_line and wait_flag==0:
+        
+        if trial_seq>0:
+            msg_flag= curr_line.split(' ')[0]
+            
+            ## add trial end flags
+            new_file.append(msg_flag + ' ENDBUTTON 5')
+            new_file.append(msg_flag + ' DISPLAY OFF')
+            new_file.append(msg_flag + ' TRIAL_RESULT 5')
+            new_file.append(msg_flag + ' TRIAL OK')
+        
+        continue # don't add the current flag so that we have one flag per trial
+    
     
     if wait_flag==1 and 'TRIALID' in curr_line:
         
@@ -68,10 +83,15 @@ for i in range(len(lines)):
         wait_flag= 0 # reset so that it doesn't get triggered in repetition
     
     
-    ## we want to filter out questions somehow?
-    
-    ## add trial end flags
-    ## add display on/off flags?
+    if 'SYNCTIME' in curr_line:
+       msg_flag= curr_line.split(' ')[0]
+       new_file.append(msg_flag + ' GAZE TARGET ON')
+       new_file.append(msg_flag + ' GAZE TARGET OFF')
+       new_file.append(msg_flag + ' DISPLAY ON')
     
     # append current line to new file once all changes have been done:
     new_file.append(curr_line)
+
+with open(file_dir + 'new/'+ file_name + "_new.asc", 'w') as f:
+    f.write('\n'.join(new_file))
+
